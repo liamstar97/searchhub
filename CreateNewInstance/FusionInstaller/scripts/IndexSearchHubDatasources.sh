202f7e4c4afec0c98a6786b275ba8c7d1e24c254
Liam Thompson <liamstar@gmaill.com>
2018-08-30 12:34:29 -0700

    changes to README

--- CreateNewInstance/FusionInstaller/scripts/IndexSearchHubDatasources.sh
+++ CreateNewInstance/FusionInstaller/scripts/IndexSearchHubDatasources.sh
@@ -6,28 +6,43 @@
 #INDEXER_STATUS="FINISHED"
 DATA_SOURCES=0
 
-#options
-options=$(getopt -o d: --long datasource-quantity -- "$@")
-[ $? -eq 0 ] || {
-    echo "exiting"
+if [ $# == 0 ]; then
+    echo "missing options, please use -h or --help to see all available options"
     exit 1
+fi
+
+function print_usage() {
+    ERROR_MSG="$1"
+
+    if [ "$ERROR_MSG" != "" ]; then
+        echo -e "\nERROR: $ERROR_MSG\n"
+    fi
 }
-eval set -- "$options"
-while true; do
-    case $1 in
+
+
+#options
+if [ $# -gt 0 ]; then
+    while true; do
+        case $1 in
             -d  | --datasource-list)
-                if [[ $2 == -* ]]; then
-                    missing_argument=${2}
+                if [[ -z "$2" || "${2:0:1}" == "-" ]]; then
+                    print_usage "a list of datasources is required when using the $1 option!"
+                    exit 1
                 fi
                 DATA_SOURCES=${2}
                 shift 2
-                ;;
-            --)
-                shift
-                break
-                ;;
-    esac
-done
+            ;;
+            *)
+                if [ "$1" != "" ]; then
+                    PASS_THRU_ARGS+=("$1")
+                    shift
+                else
+                    break
+              fi
+            ;;
+        esac
+    done
+fi
 
 if [ "${missing_argument}" != "" ]; then
     echo "${missing_argument} requires an argument please use -h or --help"
